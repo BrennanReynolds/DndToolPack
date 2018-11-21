2b1934a3c8faffe5a23a71a63385dd47df2e2ba1
Brennan Reynolds <brennanj@protonmail.com>
2018-11-21 15:15:07 -0600

    Added super Basic counting program in test package

--- DNDToolpack/src/test/kotlin/crazytestview.kt
+++ DNDToolpack/src/test/kotlin/crazytestview.kt
@@ -1,5 +1,9 @@
 
 
+import javafx.beans.property.SimpleDoubleProperty
+import javafx.beans.property.SimpleIntegerProperty
+import javafx.scene.control.ProgressBar
+import javafx.scene.control.ProgressIndicator
 import javafx.scene.layout.AnchorPane
 import tornadofx.*
 
@@ -7,6 +11,28 @@ import tornadofx.*
 
 class crazytestview : View("My View") {
     override val root : AnchorPane by fxml()
+
+    val counter = SimpleDoubleProperty(0.0)
+    val testbar: ProgressBar by fxid()
+    val testcircle: ProgressIndicator by fxid()
+
+    init{
+
+        testcircle.bind(counter)
+        testbar.bind(counter)
+
+
+    }
+
+    fun increment(){
+
+        if(counter.value > 1.0){
+            counter.value = 0.0
+            println("reset!")
+        }
+        else counter.value += 0.1
+    }
+
 
 
 }
