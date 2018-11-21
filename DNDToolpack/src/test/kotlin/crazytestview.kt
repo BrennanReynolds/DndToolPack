a8d9ab9c1296cb712d075711ae1ec1585e543b7d
Brennan Reynolds <brennanj@protonmail.com>
2018-11-21 14:43:42 -0600

    Test files for basic view

--- DNDToolpack/src/test/kotlin/crazytestview.kt
+++ DNDToolpack/src/test/kotlin/crazytestview.kt
@@ -1,6 +1,9 @@
+
+
 import javafx.scene.layout.AnchorPane
-import javafx.scene.layout.HBox
 import tornadofx.*
+
+
 
 class crazytestview : View("My View") {
     override val root : AnchorPane by fxml()
